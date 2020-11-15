98c72f32b89041f11777854bf4dd9ba2f3205727
Valeriooooh <diogocsvalerio@protonmail.com>
2020-11-15 18:00:01 +0000

    finally

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FlutterShare',
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: Text('és rei misas'),
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,8 +7,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //bool = isAuth = false;
+
+  buildAuthScreen() {
+    return Text('Authenticated');
+  }
+
+  buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Forum De Duvidas',
+              style: TextStyle(fontSize: 90.0, color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Home");
+    //return isAuth ? buildAuthScreen() : buildUnAuthScreen();
   }
 }
